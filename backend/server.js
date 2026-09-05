2a3899b485f3cdb10cc6b7f70d530710de120d5e
vishaldhiman15 <vishal1591.be23@chitkarauniversity.edu.in>
2026-09-05 14:16:05 +0530

    Configure backend for Vercel Serverless

--- backend/server.js
+++ backend/server.js
@@ -28,4 +28,8 @@ app.use('/api/users', userRoutes)
 app.get('/api/health', (req, res) => res.send('OK'))
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+}
+
+module.exports = app
